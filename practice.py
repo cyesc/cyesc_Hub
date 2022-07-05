68df0878889971dc7910bb0a534fdff0435439a6
cyesc <csc92111@gmail.com>
2022-07-05 22:56:30 +0900

    commit

--- practice.py
+++ practice.py
@@ -132,7 +132,123 @@
 #(출력문 예제)
 #오프라인 스터디 모임 날짜는 매월 x 일로 선정되었습니다.
 
-from random import *
-# date = int(random() * 29)
-date = randint(4,28)
-print("오프라인 스터디 모임 날짜는 매월 " + str(date) + "일로 선정되었습니다.")
+# from random import *
+# # date = int(random() * 29)
+# date = randint(4,28)
+# print("오프라인 스터디 모임 날짜는 매월 " + str(date) + "일로 선정되었습니다.")
+
+
+## 문자열
+# sentence = '나는 소년입니다'
+# print(sentence)
+# sentence2 = "파이썬은 쉬워요"
+# print(sentence2)
+# sentence3 = """
+# 나는 소년이고,
+# 파이썬은 쉬워요
+# """
+# print(sentence3)
+
+
+## 슬라이싱
+# jumin = "921102-1234567"
+# print("성별 : " + jumin[7])
+# print("연 : " + jumin[0:2]) #0자리부터 2직전까지
+# print("월 : " + jumin[2:4])
+# print("일 : " + jumin[4:6])
+# print("생년월일 : " + jumin[:6]) #처음부터 6직전까지
+# print("뒤 7자리 : " + jumin[7:])
+# print("뒤 7자리(뒤에서부터) : " + jumin[-7:])
+
+
+## 문자열처리함수
+# python = "Python is Amazing"
+# print(python.lower())
+# print(python.upper())
+# print(python[0].isupper())
+# print(len(python))
+# print(python.replace("Python", "Java"))
+
+# index = python.index("n")
+# print(index)
+# index = python.index("n", index + 1) #n의 두번째 위치
+# print(index)
+
+# print(python.find("Java"))
+# # print(python.index("Java")) #index에서 없는 문자를 찾을 경우 에러남
+# print("hi")
+
+# print(python.count("n"))
+
+
+## 문자열포맷
+# print("a" + "b")
+# print("a", "b")
+
+# #방법1
+# print("나는 %d살입니다." % 20)
+# print("니는 %s을 좋아해요." % "파이썬")
+# print("Apple 은 %c로 시작해요." % "A")
+# # %s
+# print("나는 %s살입니다." % 20)
+# print("나는 %s색과 %s색을 좋아해요" % ("파란", "빨간"))
+
+# #방법2
+# print("나는 {}살입니다." .format(20))
+# print("나는 {}색과 {}색을 좋아해요" .format("파란", "빨간"))
+# print("나는 {0}색과 {1}색을 좋아해요" .format("파란", "빨간"))
+# print("나는 {1}색과 {0}색을 좋아해요" .format("파란", "빨간"))
+
+# #방법3
+# print("나는 {age}살이며, {color}색을 좋아해요." .format(age = 20, color = "빨간"))
+# print("나는 {age}살이며, {color}색을 좋아해요." .format(color = "빨간", age = 20))
+
+# #방법4 (v3.6이상 ~)
+# age = 20
+# color = "빨간"
+# print(f"나는 {age}살이며, {color}색을 좋아해요.")
+
+
+## 탈출문자
+# print("백문의 불여일견\n백견이 불여일타")
+
+# \" \" : 문장 내에서 따옴표
+#저는 "나도코딩" 입니다.
+# print("저는 '나도코딩' 입니다.")
+# print('저는 "나도코딩" 입니다.')
+# print("저는 \"나도코딩\" 입니다.")
+
+# \\ : 문장 내에서 \
+# print("C:\\Users\\user\\Desktop\\PythonWorkspace\\cyesc_Hub>")
+
+# \r : 커서를 맨 앞으로 이동
+# print("Red Apple\rPine")
+
+# \b : 백스페이스 (한 글자 삭제)
+# print("Redd\bApple")
+
+# \t : 탭
+# print("Red\tApple")
+
+
+## Quiz
+#Quiz) 사이트별로 비밀번호를 만들어 주는 프로그램을 작성하시오
+#예) http://naver.com
+#규칙1 : http:// 부분은 제외 => naver.com
+#규칙2 : 처음 만나는 점(.) 이후 부분은 제외 => naver
+#규칙3 : 남은 글자 중 처음 세자리 + 글자 갯수 + 글자 내 'e' 갯수 + "!" 로 구성
+#                (nav)               (5)             (1)                 (!)
+#예) 생성된 비밀번호 : nav51!
+
+# url = "http://naver.com"
+# url = url[7:12]
+# print("생성된 비밀번호 : " + str(url[0:3]) + str(len(url)) + str(url.count("e")) + "!")
+
+# url = "http://naver.com"
+# print("생성된 비밀번호 : " + str(url[7:10]) + str(len(url[7:12])) + str(url[7:12].count("e")) + "!")
+
+url = "http://naver.com"
+my_str = url.replace("http://", "")
+my_str = my_str[:my_str.index(".")]
+password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+print("생성된 비밀번호 : " + password)
